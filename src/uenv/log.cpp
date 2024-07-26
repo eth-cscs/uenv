4e0cb6a4e1d4ee36cbefe1440f29726f1e4a8789
Mikael Simberg <mikael.simberg@iki.fi>
2024-07-26 13:07:34 +0200

    Explicitly set log level of combined default logger to trace to pass through all messages to subloggers

--- src/uenv/log.cpp
+++ src/uenv/log.cpp
@@ -16,8 +16,13 @@ void init_log(spdlog::level::level_enum console_log_level,
         "uenv", LOG_PID, 0, false);
     syslog_sink->set_level(syslog_log_level);
 
+    // The default logger is a combined logger that dispatches to the console
+    // and syslog. We explicitly set the level to trace to allow all messages to
+    // pass through the combined logger. The lower level loggers will filter
+    // messages at their level.
     spdlog::set_default_logger(std::make_shared<spdlog::logger>(
         "uenv", spdlog::sinks_init_list({console_sink, syslog_sink})));
+    spdlog::set_level(spdlog::level::trace);
     spdlog::set_pattern("[%Y-%m-%d %H:%M:%S.%e] [%n] [%^%l%$] %v");
 }
 } // namespace uenv
