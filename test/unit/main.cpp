bce136b8238f7a5b9d780535e555e37594c0f124
Mikael Simberg <mikael.simberg@iki.fi>
2024-07-26 11:48:31 +0200

    Initialize logs for unit tests

--- test/unit/main.cpp
+++ test/unit/main.cpp
@@ -1,2 +1,10 @@
 #define CATCH_CONFIG_MAIN
 #include <catch2/catch_all.hpp>
+
+#include <uenv/log.h>
+
+static struct unit_init_log {
+    unit_init_log() {
+        uenv::init_log();
+    }
+} uil{};
