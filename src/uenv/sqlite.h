4f1aa38f95e195a69a03b2135ad8a37348beee88
Simon Pintarelli <simon.pintarelli@cscs.ch>
2024-08-28 18:15:46 +0200

    clang format sqlite

--- src/uenv/sqlite.h
+++ src/uenv/sqlite.h
@@ -9,66 +9,71 @@ struct sqlite3;
 enum class sqlite_open : int { readonly };
 
 class SQLiteError : public std::exception {
-public:
-  SQLiteError(const std::string &msg) : msg(msg) {}
-  const char *what() const noexcept override { return msg.c_str(); }
+  public:
+    SQLiteError(const std::string& msg) : msg(msg) {
+    }
+    const char* what() const noexcept override {
+        return msg.c_str();
+    }
 
-private:
-  std::string msg;
+  private:
+    std::string msg;
 };
 
 class SQLiteStatement;
 
 class SQLiteDB {
-public:
-  SQLiteDB(const std::string &fname, sqlite_open flag);
-  SQLiteDB(const SQLiteDB &) = delete;
-  SQLiteDB operator=(const SQLiteDB &) = delete;
-  virtual ~SQLiteDB();
+  public:
+    SQLiteDB(const std::string& fname, sqlite_open flag);
+    SQLiteDB(const SQLiteDB&) = delete;
+    SQLiteDB operator=(const SQLiteDB&) = delete;
+    virtual ~SQLiteDB();
 
-private:
-  sqlite3 *db{nullptr};
+  private:
+    sqlite3* db{nullptr};
 
-protected:
-  sqlite3 *get() { return db; }
+  protected:
+    sqlite3* get() {
+        return db;
+    }
 
-  friend SQLiteStatement;
+    friend SQLiteStatement;
 };
 
 class SQLiteColumn;
 
 class SQLiteStatement {
-public:
-  SQLiteStatement(SQLiteDB &db, const std::string &query);
-  SQLiteStatement(const SQLiteStatement &) = delete;
-  SQLiteStatement operator=(const SQLiteStatement &) = delete;
-  std::string getColumnType(int i) const;
-  SQLiteColumn getColumn(int i) const;
-  int getColumnIndex(const std::string &name) const;
-  void bind(const std::string &name, const std::string &value);
-  bool execute();
+  public:
+    SQLiteStatement(SQLiteDB& db, const std::string& query);
+    SQLiteStatement(const SQLiteStatement&) = delete;
+    SQLiteStatement operator=(const SQLiteStatement&) = delete;
+    std::string getColumnType(int i) const;
+    SQLiteColumn getColumn(int i) const;
+    int getColumnIndex(const std::string& name) const;
+    void bind(const std::string& name, const std::string& value);
+    bool execute();
 
-  virtual ~SQLiteStatement();
+    virtual ~SQLiteStatement();
 
-private:
-  void checkIndex(int i) const;
+  private:
+    void checkIndex(int i) const;
 
-private:
-  SQLiteDB &db;
-  sqlite3_stmt *stmt;
-  int column_count{-1};
-  int rc;
-  friend SQLiteColumn;
+  private:
+    SQLiteDB& db;
+    sqlite3_stmt* stmt;
+    int column_count{-1};
+    int rc;
+    friend SQLiteColumn;
 };
 
 class SQLiteColumn {
-public:
-  SQLiteColumn(const SQLiteStatement &statement, int index);
-  std::string name() const;
-  operator int() const;
-  operator std::string() const;
+  public:
+    SQLiteColumn(const SQLiteStatement& statement, int index);
+    std::string name() const;
+    operator int() const;
+    operator std::string() const;
 
-private:
-  const SQLiteStatement &statement;
-  const int index;
+  private:
+    const SQLiteStatement& statement;
+    const int index;
 };
