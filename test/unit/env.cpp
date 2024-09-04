c51e510d305ed0dcd9ad15963ba2eb380139e133
bcumming <bcumming@cscs.ch>
2024-09-04 13:51:17 +0200

    correctly handle null description in uenv meta; add basic test

--- test/unit/env.cpp
+++ test/unit/env.cpp
@@ -2,3 +2,10 @@
 #include <fmt/core.h>
 
 #include <uenv/env.h>
+#include <uenv/log.h>
+#include <uenv/meta.h>
+
+TEST_CASE("load_meta", "[env]") {
+    REQUIRE(uenv::load_meta("/home/bcumming/software/uenv2/test/data/env-files/"
+                            "cp2k-2024.2-v1.json"));
+}
