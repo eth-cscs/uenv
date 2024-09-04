0c74cee17a6438d66153acca53b8d7dccd38a17f
bcumming <bcumming@cscs.ch>
2024-09-04 14:13:08 +0200

    fix relative path in unit tests

--- test/unit/env.cpp
+++ test/unit/env.cpp
@@ -6,12 +6,5 @@
 #include <uenv/meta.h>
 
 TEST_CASE("load_meta", "[env]") {
-    auto x = uenv::load_meta("/home/bcumming/software/uenv2/test/data/"
-                             "env-files/cp2k-2024.2-v1.json");
-    if (!x)
-        fmt::println("ERROR: {}", x.error());
-    else
-        fmt::println("FINE");
-    REQUIRE(uenv::load_meta("/home/bcumming/software/uenv2/test/data/env-files/"
-                            "cp2k-2024.2-v1.json"));
+    REQUIRE(uenv::load_meta("../test/data/env-files/cp2k-2024.2-v1.json"));
 }
