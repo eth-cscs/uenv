48ce0302db9fc8a8a79d99db0fd015afaa54bf7a
bcumming <bcumming@cscs.ch>
2024-12-04 11:42:11 +0100

    tune the install script

--- install-alps-local.sh
+++ install-alps-local.sh
@@ -1,12 +1,33 @@
 #!/bin/bash
 
+set -e
+
 root=$(cd -- "$( dirname -- "${BASH_SOURCE[0]}" )" &> /dev/null && pwd)
 build=$root/build-alps
 pyenv=$root/pyenv-alps
-python3 -m venv $pyenv
-source $pyenv/bin/activate
-pip install --upgrade pip
-pip install meson ninja
-CC=gcc-12 CXX=g++-12 meson setup --prefix=$HOME/.local $build $root
-meson compile -C$build
-meson install -C$build --skip-subprojects
+log=$root/log_local
+
+echo "== use python to install up meson and ninja"
+python3 -m venv $pyenv > $log
+source $pyenv/bin/activate >> $log
+pip install --upgrade pip >> $log
+pip install meson ninja >> $log
+echo "== configure"
+CC=gcc-12 CXX=g++-12 meson setup --prefix=$HOME/.local $build $root >> $log
+echo "== build"
+meson compile -C$build >> $log
+echo "== install"
+meson install -C$build --skip-subprojects >> $log
+
+echo ""
+echo "== succesfully installed"
+
+echo "======================================================================================="
+echo "add the following to your ~/.bashrc (or equivalent for .zshrc):"
+echo ""
+echo "Note:"
+echo "- log out and in again for the changes to take effect"
+echo "- if successful, 'uenv --version' will report version 6"
+echo ""
+echo "export PATH=$HOME/.local/bin"
+echo "unset -f uenv"
