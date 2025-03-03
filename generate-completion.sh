8fd66a9b59a1b3e755b6af60784984f1880ed1f3
Radim Janalík <radim.janalik@gmail.com>
2025-03-03 23:59:15 +0100

    Revert "Create completion scripts in meson install"
    This reverts commit 1a46012963b9d14fc81c1ce5b36af3998e52556d.

--- generate-completion.sh
+++
@@ -1,4 +0,0 @@
-#!/bin/bash
-
-echo "Creating bash completion script in ${MESON_INSTALL_PREFIX}"
-PATH=${MESON_INSTALL_PREFIX}/bin:$PATH uenv completion bash > ${MESON_INSTALL_PREFIX}/uenv-bash-completion.sh
