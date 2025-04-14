b03bd9b9bf8294e8d078d76ecb9a8819bc212cc6
Ben Cumming <bcumming@cscs.ch>
2025-04-14 19:08:50 +0200

    warn if no env provided in view descriptions (#71)
    fixes silently not loading views on old uenv

--- src/uenv/meta.cpp
+++ src/uenv/meta.cpp
@@ -56,65 +56,77 @@ util::expected<meta, std::string> load_meta(const std::filesystem::path& file) {
         // about that without resorting to JSON schema.
         std::unordered_map<std::string, concrete_view> views;
         if (auto& jviews = raw["views"]; jviews.is_object()) {
-            for (auto& [name, desc] : jviews.items()) {
+            for (auto& [view_name, desc] : jviews.items()) {
                 envvars::patch envvars;
-                if (auto& list = desc["env"]["values"]["list"];
-                    list.is_object()) {
-                    for (auto& [var_name, updates] : list.items()) {
-                        for (auto& u : updates) {
-                            // if "op" is not one of "set", "append", "prepend"
-                            // or "unset", the default "unset" will be selected.
-                            if (u.contains("op") && u.contains("value")) {
-                                const envvars::update_kind op =
-                                    u["op"] == "append"
-                                        ? envvars::update_kind::append
-                                    : u["op"] == "prepend"
-                                        ? envvars::update_kind::prepend
-                                    : u["op"] == "set"
-                                        ? envvars::update_kind::set
-                                        : envvars::update_kind::unset;
-                                if (u["value"].is_array()) {
-                                    std::vector<std::string> paths = u["value"];
-                                    envvars.update_prefix_path(var_name,
-                                                               {op, paths});
+                const bool has_env = desc.contains("env");
+                if (!has_env) {
+                    spdlog::warn(
+                        "uenv::load_meta view '{}:{}' contains only an "
+                        "activation script - the view will not load "
+                        "correctly with this version of uenv",
+                        name, view_name);
+                } else {
+                    const auto& env = desc["env"];
+                    if (auto& list = env["values"]["list"]; list.is_object()) {
+                        for (auto& [var_name, updates] : list.items()) {
+                            for (auto& u : updates) {
+                                // if "op" is not one of "set", "append",
+                                // "prepend" or "unset", the default "unset"
+                                // will be selected.
+                                if (u.contains("op") && u.contains("value")) {
+                                    const envvars::update_kind op =
+                                        u["op"] == "append"
+                                            ? envvars::update_kind::append
+                                        : u["op"] == "prepend"
+                                            ? envvars::update_kind::prepend
+                                        : u["op"] == "set"
+                                            ? envvars::update_kind::set
+                                            : envvars::update_kind::unset;
+                                    if (u["value"].is_array()) {
+                                        std::vector<std::string> paths =
+                                            u["value"];
+                                        envvars.update_prefix_path(var_name,
+                                                                   {op, paths});
+                                    } else {
+                                        spdlog::error("invalid prefix_list "
+                                                      "value: expect an "
+                                                      "array of strings: '{}'",
+                                                      var_name);
+                                    }
                                 } else {
+                                    // create an error if an invalid value was
+                                    // provided, but don't exit
                                     spdlog::error(
-                                        "invalid prefix_list value: expect an "
-                                        "array of strings: '{}'",
+                                        "invalid prefix_list env variable "
+                                        "definition for '{}'",
                                         var_name);
                                 }
+                            }
+                        }
+                    }
+                    if (auto& scalar = env["values"]["scalar"];
+                        scalar.is_object()) {
+                        for (auto& [var_name, val] : scalar.items()) {
+                            if (val.is_null()) {
+                                envvars.update_scalar(var_name, std::nullopt);
+                            } else if (val.is_string()) {
+                                envvars.update_scalar(var_name, val);
                             } else {
                                 // create an error if an invalid value was
                                 // provided, but don't exit
                                 spdlog::error(
-                                    "invalid prefix_list env variable "
-                                    "definition for '{}'",
-                                    var_name);
+                                    "invalid scalar environment variable value "
+                                    "(must be string or null) '{}={}'",
+                                    var_name, std::string(val));
                             }
-                        }
-                    }
-                }
-                if (auto& scalar = desc["env"]["values"]["scalar"];
-                    scalar.is_object()) {
-                    for (auto& [var_name, val] : scalar.items()) {
-                        if (val.is_null()) {
-                            envvars.update_scalar(var_name, std::nullopt);
-                        } else if (val.is_string()) {
-                            envvars.update_scalar(var_name, val);
-                        } else {
-                            // create an error if an invalid value was provided,
-                            // but don't exit
-                            spdlog::error(
-                                "invalid scalar environment variable value "
-                                "(must be string or null) '{}={}'",
-                                var_name, std::string(val));
                         }
                     }
                 }
 
                 const std::string description =
                     desc.contains("description") ? desc["description"] : "";
-                views[name] = concrete_view{name, description, envvars};
+                views[view_name] =
+                    concrete_view{view_name, description, envvars};
             }
         }
 
