43e1fe3a90d22b0a7ef1aaf66995c22aa93d9dd2
Simon Pintarelli <simon.pintarelli@cscs.ch>
2025-07-30 10:04:54 +0200

    install dependencies only if needed

--- install-alps-local.sh
+++ install-alps-local.sh
@@ -4,36 +4,46 @@ set -eu
 
 # build static dependencies
 (
-	  cd extern/
-	  (
-		    curl -L https://github.com/facebook/zstd/releases/download/v1.5.7/zstd-1.5.7.tar.gz | tar xzf -
-		    cd zstd-1.5.7/build/meson
-		    CXX=g++-12 CC=gcc-12 uvx --with=meson,ninja meson setup --wipe --buildtype=release -Dbin_programs=true -Dbin_contrib=true --prefix=$PWD/../../install builddir
-		    uvx --with=meson,ninja ninja -C builddir install
-		    cd ../../install
-		    # squashfuse expects a lib dir
-		    ln -sf lib64 lib
-	  )
+    cd extern/
+    if [ ! -d "zstd-1.5.7/install/lib/pkgconfig" ]; then
+        (
+            curl -L https://github.com/facebook/zstd/releases/download/v1.5.7/zstd-1.5.7.tar.gz | tar xzf -
+            cd zstd-1.5.7/build/meson
+            CXX=g++-12 CC=gcc-12 uvx --with=meson,ninja \
+                       meson setup \
+                       --buildtype=release \
+                       -Dbin_programs=true \
+                       -Dbin_contrib=true \
+                       --prefix=$PWD/../../install \
+                       --wipe builddir
+            uvx --with=meson,ninja ninja -C builddir install
+            cd ../../install
+            # squashfuse expects a lib dir
+            ln -sf lib64 lib
+        )
+    fi
 
-	  export ZSTD=$(realpath zstd-1.5.7/install)
+    export ZSTD=$(realpath zstd-1.5.7/install)
 
-	  (
-		    curl -L https://github.com/vasi/squashfuse/releases/download/0.6.1/squashfuse-0.6.1.tar.gz | tar xzf -
-		    cd squashfuse-0.6.1
+    if [ ! -d "squashfuse-0.6.1/install/lib/pkgconfig" ]; then
+        (
+            curl -L https://github.com/vasi/squashfuse/releases/download/0.6.1/squashfuse-0.6.1.tar.gz | tar xzf -
+            cd squashfuse-0.6.1
 
-        # patch includedir in pkg-config
-        sed -i 's/^includedir=.*/includedir=@includedir@/' squashfuse.pc.in
-        sed -i 's/^includedir=.*/includedir=@includedir@/' squashfuse_ll.pc.in
+            # patch includedir in pkg-config
+            sed -i 's/^includedir=.*/includedir=@includedir@/' squashfuse.pc.in
+            sed -i 's/^includedir=.*/includedir=@includedir@/' squashfuse_ll.pc.in
 
-		    ./configure --disable-high-level \
-			              --enable-static=yes \
-			              --enable-shared=no \
-			              --without-lzo \
-			              --without-lz4 \
-			              --with-zstd=$ZSTD \
-			              --prefix=$PWD/install
-		    make install
-	  )
+            ./configure --disable-high-level \
+                --enable-static=yes \
+                --enable-shared=no \
+                --without-lzo \
+                --without-lz4 \
+                --with-zstd=$ZSTD \
+                --prefix=$PWD/install
+            make install
+        )
+    fi
 )
 
 PKG_CONFIG_PATH=$(realpath extern/zstd-1.5.7/install/lib/pkgconfig):$(realpath extern/squashfuse-0.6.1/install/lib/pkgconfig):${PKG_CONFIG_PATH:-}
@@ -42,7 +52,7 @@ export PKG_CONFIG_PATH
 arch=$(uname -m)
 echo "== architecture: $arch"
 
-root=$(cd -- "$( dirname -- "${BASH_SOURCE[0]}" )" &> /dev/null && pwd)
+root=$(cd -- "$(dirname -- "${BASH_SOURCE[0]}")" &>/dev/null && pwd)
 build=$root/build-alps-$arch
 pyenv=$root/pyenv-alps-$arch
 install=$HOME/.local/$arch
