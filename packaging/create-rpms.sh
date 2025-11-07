ec4e46d771ecf7fb4534a11e6298ee753eeaafa0
bcumming <bcumming@cscs.ch>
2025-11-07 15:37:44 +0100

    update upload scripts

--- packaging/create-rpms.sh
+++ packaging/create-rpms.sh
@@ -4,13 +4,16 @@ set -euo pipefail
 
 #rm -rf ./artifacts
 
+oslist='15.5 15.6'
+slurmlist='23.02.7 24.05.4 24.05.8 25.05.4'
+
 arch=$(uname -m)
-for os in 15.5 15.6
+for os in $oslist
 do
     echo "SLES $os"
     container=uenv-sles$os
     podman build -f ./dockerfiles/sles$os . -t $container >& log-$os
-    for slurm in 23.02.7 24.05.4 24.05.8 25.05.0
+    for slurm in $slurmlist
     do
         echo "    Slurm version $slurm"
         podman run -v $(pwd):/work:rw -w /work -v $(realpath ..):/source:rw \
@@ -20,6 +23,3 @@ do
     done
 done
 
-#curl -u$USER:$TOKEN -XPUT \
-#    https://jfrog.svc.cscs.ch/artifactory/cscs-opensuse-${os}-rpm/uenv/${arch}/uenv-9.0.0-${slurm}.rpm \
-#    --upload-file rpms/${arch}/uenv-9.0.0-${slurm}.rpm
--- packaging/upload.sh
+++ packaging/upload.sh
@@ -3,10 +3,14 @@
 version=$(cat ../VERSION)
 token=$(cat ~/.ssh/jfrog_token)
 arch=$(uname -m)
-for os in 15.5 15.6
+
+oslist='15.5 15.6'
+slurmlist='23.02.7 24.05.4 24.05.8 25.05.4'
+
+for os in $oslist
 do
     echo "SLES $os"
-    for slurm in 23.02.7 24.05.4 24.05.8 25.05.0
+    for slurm in $slurmlist
     do
         rpm=uenv-${version}-${slurm}.${arch}.rpm
         echo "    Slurm version $slurm: $rpm"
