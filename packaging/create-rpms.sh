ba5ed5bc59fd4f15502ff0ccaf85aafc7cf21e10
bcumming <bcumming@cscs.ch>
2025-11-25 17:42:54 +0100

    use uenv build containers for packaging

--- packaging/create-rpms.sh
+++ packaging/create-rpms.sh
@@ -7,19 +7,22 @@ set -euo pipefail
 oslist='15.5 15.6'
 slurmlist='23.02.7 24.05.4 24.05.8 25.05.4'
 
+echo "==== creating RPMS for uenv $(cat ../VERSION)"
 arch=$(uname -m)
 for os in $oslist
 do
     echo "SLES $os"
-    container=uenv-sles$os
-    podman build -f ./dockerfiles/sles$os . -t $container >& log-$os
+    container=ghcr.io/bcumming/uenv-build-sles$os:$arch
+    echo "pulling $container..."
+    podman pull $container
+    #container=uenv-sles$os
+    #podman build -f ./dockerfiles/sles$os . -t $container >& log-$os
     for slurm in $slurmlist
     do
         echo "    Slurm version $slurm"
         podman run -v $(pwd):/work:rw -w /work -v $(realpath ..):/source:rw \
-            -it $container:latest                                           \
-            sh -c "./suse-build.sh --slurm-version=$slurm --ref=HEAD --os=$os" \
-                >& log-$os-$slurm
+            -it $container sh -c "./suse-build.sh --slurm-version=$slurm --ref=HEAD --os=$os" \
+                >& log-$os-$slurm-$arch
     done
 done
 
