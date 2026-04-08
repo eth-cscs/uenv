060c665dba965bdae31524b9b3f1a29ca64142a9
bcumming <bcumming@cscs.ch>
2026-04-08 13:32:58 +0200

    format

--- src/cli/uenv.cpp
+++ src/cli/uenv.cpp
@@ -122,7 +122,7 @@ int main(int argc, char** argv) {
     if (auto full_config = uenv::load_config(cli_config, cli_repo_labels,
                                              settings.calling_environment)) {
         // print any warnings that were generated while loading configuration
-        for (const auto& warning: full_config->warnings) {
+        for (const auto& warning : full_config->warnings) {
             term::warn("{}", warning);
         }
         // generate_configuration applies checks to ensure that paths in the
