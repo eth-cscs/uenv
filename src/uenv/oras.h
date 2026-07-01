08ef9ff060710fb9ca761994f5bd1bf9482d105d
bcumming <bcumming@cscs.ch>
2026-07-01 12:02:42 +0200

    remove stale oras wrappers

--- src/uenv/oras.h
+++ src/uenv/oras.h
@@ -32,24 +32,6 @@ struct error {
     };
 };
 
-bool pull(std::string rego, std::string nspace);
-
-util::expected<std::vector<std::string>, error>
-discover(const std::string& registry, const std::string& nspace,
-         const uenv_record& uenv,
-         const std::optional<credentials> token = std::nullopt);
-
-util::expected<void, error>
-pull_digest(const std::string& registry, const std::string& nspace,
-            const uenv_record& uenv, const std::string& digest,
-            const std::filesystem::path& destination,
-            const std::optional<credentials> token = std::nullopt);
-
-util::expected<void, error>
-pull_tag(const std::string& registry, const std::string& nspace,
-         const uenv_record& uenv, const std::filesystem::path& destination,
-         const std::optional<credentials> token = std::nullopt);
-
 util::expected<void, error>
 push_tag(const std::string& registry, const std::string& nspace,
          const uenv_label& label, const std::filesystem::path& source,
