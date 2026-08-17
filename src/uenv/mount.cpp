eb699b8ae4dc7885ba3bdfcd0eb901f46e59a711
Simon Pintarelli <simon.pintarelli@cscs.ch>
2026-08-17 16:52:12 +0200

    remove util-linux 2.42 workaround

--- src/uenv/mount.cpp
+++ src/uenv/mount.cpp
@@ -231,9 +231,6 @@ do_mount(const std::vector<mount_pair>& mount_entries) {
 
         auto cxt = mnt_new_context();
 
-        if (mnt_context_force_unrestricted(cxt) != 0) {
-            return util::unexpected("mnt_context_force_unrestricted failed");
-        }
 
         if (mnt_context_disable_mtab(cxt, 1) != 0) {
             return util::unexpected("Failed to disable mtab");
