75276e485338e12db07793022e1710a7d9a1577c
Simon Pintarelli <simon.pintarelli@cscs.ch>
2026-08-19 11:03:11 +0200

    remove curly braces around single lines

--- src/uenv/rootless.cpp
+++ src/uenv/rootless.cpp
@@ -35,9 +35,8 @@ util::expected<void, std::string> unshare_mount_map_root() {
 
     if (auto r =
             mount(std::nullopt, "/", std::nullopt, MS_SHARED | MS_REC, nullptr);
-        !r) {
+        !r)
         return r;
-    }
 
     // map current uid to root
     char buf[256];
@@ -45,18 +44,16 @@ util::expected<void, std::string> unshare_mount_map_root() {
     if (!proc_uid_map)
         return util::unexpected(proc_uid_map.error());
     snprintf(buf, sizeof(buf), "0 %d 1", uid);
-    if (auto r = write(proc_uid_map.value(), buf, strlen(buf)); !r) {
+    if (auto r = write(proc_uid_map.value(), buf, strlen(buf)); !r)
         return r;
-    }
     Z_e(close(proc_uid_map.value()));
 
     // write /proc/self/gid_setgroups -> deny
     auto proc_setgroups = openat(AT_FDCWD, "/proc/self/setgroups", O_WRONLY);
     if (!proc_setgroups)
         return util::unexpected(proc_setgroups.error());
-    if (auto r = write(proc_setgroups.value(), "deny", 4); !r) {
+    if (auto r = write(proc_setgroups.value(), "deny", 4); !r)
         return r;
-    }
     Z_e(close(proc_setgroups.value()));
 
     // map gid  to root group
@@ -64,16 +61,14 @@ util::expected<void, std::string> unshare_mount_map_root() {
     if (!proc_gid_map)
         return util::unexpected(proc_gid_map.error());
     snprintf(buf, sizeof(buf), "0 %d 1", gid);
-    if (auto r = write(proc_gid_map.value(), buf, strlen(buf)); !r) {
+    if (auto r = write(proc_gid_map.value(), buf, strlen(buf)); !r)
         return r;
-    }
     Z_e(close(proc_gid_map.value()));
 
     // the following is executed by `unshare --mount --map-root-user`
     if (auto r = mount("none", "/", std::nullopt, MS_REC | MS_PRIVATE, nullptr);
-        !r) {
+        !r)
         return r;
-    }
     return {};
 }
 
@@ -88,9 +83,8 @@ util::expected<void, std::string> map_effective_user(uid_t uid, gid_t gid) {
     if (!proc_uid_map)
         return util::unexpected(proc_uid_map.error());
     sprintf(buf, "%d 0 1", uid);
-    if (auto r = write(proc_uid_map.value(), buf, strlen(buf)); !r) {
+    if (auto r = write(proc_uid_map.value(), buf, strlen(buf)); !r)
         return r;
-    }
     Z_e(close(proc_uid_map.value()));
 
     // note: setgroups is already "deny" here, inherited from the enclosing
@@ -102,9 +96,8 @@ util::expected<void, std::string> map_effective_user(uid_t uid, gid_t gid) {
     if (!proc_gid_map)
         return util::unexpected(proc_gid_map.error());
     sprintf(buf, "%d 0 1", gid);
-    if (auto r = write(proc_gid_map.value(), buf, strlen(buf)); !r) {
+    if (auto r = write(proc_gid_map.value(), buf, strlen(buf)); !r)
         return r;
-    }
     Z_e(close(proc_gid_map.value()));
 
     // disable coredump again (slurm policy)
@@ -115,13 +108,11 @@ util::expected<void, std::string> map_effective_user(uid_t uid, gid_t gid) {
 }
 
 util::expected<void, std::string> exit_sandbox(uid_t uid, gid_t gid) {
-    if (auto r = map_effective_user(uid, gid); !r) {
+    if (auto r = map_effective_user(uid, gid); !r)
         return r;
-    }
 
-    if (prctl(PR_SET_NO_NEW_PRIVS, 1, 0, 0, 0) != 0) {
+    if (prctl(PR_SET_NO_NEW_PRIVS, 1, 0, 0, 0) != 0)
         return util::unexpected("PR_SET_NO_NEW_PRIVS failed");
-    }
     return {};
 }
 
@@ -163,15 +154,13 @@ util::expected<void, std::string> do_sqfs_ll_mount(const mount_pair& entry,
     spdlog::trace("do_sqfs_ll_mount");
     // use a pipe to synchronize parent and child process
     auto rf = util::ready_fork::create();
-    if (!rf) {
+    if (!rf)
         return util::unexpected(rf.error());
-    }
 
     pid_t pid = rf->fork();
-    if (pid < 0) {
+    if (pid < 0)
         return util::unexpected(
             fmt::format("fork() failed: {}", strerror(errno)));
-    }
 
     if (pid == 0) {
 
@@ -203,12 +192,11 @@ util::expected<void, std::string> do_sqfs_ll_mount(const mount_pair& entry,
         // we must get a different fd.
         while (true) {
             int fd = open("/dev/null", O_RDONLY);
-            if (fd == -1) {
+            if (fd == -1)
                 // Can't open /dev/null, how bizarre! However,
                 // fuse_deamonize won't clobber fds if it can't
                 // open /dev/null either, so we ought to be OK.
                 break;
-            }
             if (fd > 2) {
                 // fds 0-2 are now guaranteed to be open.
                 close(fd);
@@ -223,9 +211,8 @@ util::expected<void, std::string> do_sqfs_ll_mount(const mount_pair& entry,
         int offset = 0;
         // OPEN FS
         err = !(ll = sqfs_ll_open(entry.sqfs.c_str(), offset));
-        if (err) {
+        if (err)
             child_fail("sqfs_ll_open failed");
-        }
         if (!err) {
             // startup fuse
             sqfs_ll_chan ch;
@@ -253,9 +240,8 @@ util::expected<void, std::string> do_sqfs_ll_mount(const mount_pair& entry,
                             err = fuse_session_loop_mt(ch.session, &config);
                         }
 #else  /* FUSE_USE_VERSION < 30 */
-                        if (!fuse_st) {
+                        if (!fuse_st)
                             err = fuse_session_loop_mt(ch.session);
-                        }
 #endif /* FUSE_USE_VERSION */
                         else
 #endif
@@ -263,12 +249,10 @@ util::expected<void, std::string> do_sqfs_ll_mount(const mount_pair& entry,
 
                         teardown_idle_timeout();
                         fuse_remove_signal_handlers(ch.session);
-                    } else {
+                    } else
                         child_fail("set signal handlers failed.");
-                    }
-                } else {
+                } else
                     child_fail("daemonize failed");
-                }
                 sqfs_ll_destroy(ll);
 
                 // Rely on OS cleanup for the actual unmount, to avoid
@@ -307,9 +291,8 @@ util::expected<void, std::string> do_sqfs_ll_mount(const mount_pair& entry,
                 }
                 }
             }
-        } else {
+        } else
             child_fail("sqfs_ll_open_failed");
-        }
         fuse_opt_free_args(&args);
         free(ll);
         exit(0);
